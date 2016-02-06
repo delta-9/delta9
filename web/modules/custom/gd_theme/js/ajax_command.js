7d5dbba6b6b27c9bfc13e49db954c29725e20bd6
vincent baronnet <delta@1270276.no-reply.drupal.org>
2016-02-07 00:36:12 +0100

    add install profil, core and theme module.

---
+++ web/modules/custom/gd_theme/js/ajax_command.js
@@ -0,0 +1,60 @@
+/*
+ JavaScript should be made compatible with libraries other than jQuery by
+ wrapping it with an "anonymous closure". See:
+ - https://drupal.org/node/1446420
+ - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+ */
+(function ($, Drupal, window, document, undefined) {
+  Drupal.ajax = Drupal.ajax || {};
+  Drupal.ajax.prototype = Drupal.ajax.prototype || {};
+  Drupal.ajax.prototype.commands = Drupal.ajax.prototype.commands || {};
+  Drupal.ajax.prototype.commands.gd_insert = function (ajax, response, status) {
+    // Get information from the response. If it is not there, default to
+    // our presets.
+    var wrapper = response.selector ? $(response.selector) : $(ajax.wrapper);
+    var method = response.method || ajax.method;
+    var effect = ajax.getEffect(response);
+
+    // If removing content from the wrapper, detach behaviors first.
+    switch (method) {
+      case 'html':
+      case 'replaceWith':
+      case 'replaceAll':
+      case 'empty':
+      case 'remove':
+        var settings = response.settings || ajax.settings || Drupal.settings;
+        Drupal.detachBehaviors(wrapper, settings);
+    }
+    var new_content = $(response.data);
+    // Add the new content to the page.
+    wrapper[method](new_content);
+
+    // Immediately hide the new content if we're using any effects.
+    if (effect.showEffect != 'show') {
+      new_content.hide();
+    }
+
+    // Determine which effect to use and what content will receive the
+    // effect, then show the new content.
+    if ($('.ajax-new-content', new_content).length > 0) {
+      $('.ajax-new-content', new_content).hide();
+      new_content.show();
+      $('.ajax-new-content', new_content)[effect.showEffect](effect.showSpeed);
+    }
+    else if (effect.showEffect != 'show') {
+      new_content[effect.showEffect](effect.showSpeed);
+    }
+
+    // Attach all JavaScript behaviors to the new content, if it was successfully
+    // added to the page, this if statement allows #ajax['wrapper'] to be
+    // optional.
+    if (new_content.parents('html').length > 0) {
+      // Apply any settings from the returned JSON if available.
+      var settings = response.settings || ajax.settings || Drupal.settings;
+      Drupal.attachBehaviors(new_content, settings);
+    }
+  };
+
+
+
+})(jQuery, Drupal, this, this.document);
